d4edd4cee2850b8a859423cedaaa6d3d5a1439d2
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-10-07 19:16:06 +0200

    Implemented DTOs and Controller Action Post Method for Categories

---
+++ Blogging.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using Blogging.Api.Models.Domain;
+using Blogging.Api.Models.Dtos.Categories;
+using Blogging.Api.Persistance;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blogging.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+
+        // 
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map Dto to domain model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            // Map domain model back to Dto
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            // The above code is quite a lengthy method which can be refactored by using 3rd party packages (AutoMapper)
+            return Ok(response);
+
+        }
+    }
+}
---
+++ Blogging.Api/Models/Dtos/Categories/CategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Blogging.Api.Models.Dtos.Categories
+{
+    public class CategoryDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UrlHandle { get; set; }
+    }
+}
