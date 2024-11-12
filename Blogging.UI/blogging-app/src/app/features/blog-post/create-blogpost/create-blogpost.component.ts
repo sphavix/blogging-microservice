7a02cf421248d5817eb979a00048b310d4d1bb39
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-11-12 20:00:16 +0200

    Image upload on add blog UI

--- Blogging.UI/blogging-app/src/app/features/blog-post/create-blogpost/create-blogpost.component.ts
+++ Blogging.UI/blogging-app/src/app/features/blog-post/create-blogpost/create-blogpost.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CreateArticleRequest } from '../models/create-blogpost-request.model';
 import { BlogPostService } from '../services/blog-post.service';
 import { Router } from '@angular/router';
 import { CategoryService } from '../../category/services/category.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Category } from '../../category/models/category.model';
+import { UploadPictureService } from 'src/app/shared/components/image-upload-selector/upload-picture.service';
 
 @Component({
   selector: 'app-create-blogpost',
   templateUrl: './create-blogpost.component.html',
   styleUrls: ['./create-blogpost.component.css']
 })
-export class CreateBlogpostComponent implements OnInit {
+export class CreateBlogpostComponent implements OnInit, OnDestroy {
   model: CreateArticleRequest;
   categories$?: Observable<Category[]>;
 
-  constructor(private articleService: BlogPostService, private categoryService: CategoryService, private router: Router) {
+
+  isImageSelectorVisible: boolean = false;
+
+  selectedPictureSubscription?: Subscription;
+
+  constructor(private articleService: BlogPostService, private categoryService: CategoryService, private router: Router, private pictureService: UploadPictureService) {
     this.model = {
       title: '',
       shortDescription: '',
@@ -29,9 +35,15 @@ export class CreateBlogpostComponent implements OnInit {
     }
   }
 
-
   ngOnInit(): void {
     this.categories$ = this.categoryService.getCategories();
+
+    this.selectedPictureSubscription = this.pictureService.onSelectedPicture().subscribe({
+      next: (response) => {
+        this.model.featureImageUrl = response.pictureUrl;
+        this.closeImageSelector();
+      }
+    })
   }
 
   onCreateBlogPost() : void {
@@ -42,4 +54,18 @@ export class CreateBlogpostComponent implements OnInit {
       }
     });
   }
+
+  openImageSelector(): void {
+    this.isImageSelectorVisible = true;
+  }
+
+  closeImageSelector(): void {
+    this.isImageSelectorVisible = false;
+  }
+
+
+  ngOnDestroy(): void {
+    this.selectedPictureSubscription?.unsubscribe();
+  }
+
 }
