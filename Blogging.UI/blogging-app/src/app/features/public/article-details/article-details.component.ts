6e87469e8995470d41468004156e55746cbe08ef
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-11-18 18:14:56 +0200

    Minor change on the details page

--- Blogging.UI/blogging-app/src/app/features/public/article-details/article-details.component.ts
+++ Blogging.UI/blogging-app/src/app/features/public/article-details/article-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BlogPostService } from '../../blog-post/services/blog-post.service';
 import { Observable } from 'rxjs';
 import { Article } from '../../blog-post/models/create-blodpost.model';
@@ -14,7 +14,7 @@ export class ArticleDetailsComponent implements OnInit {
   url: string | null = null;
   article$?: Observable<Article>;
 
-  constructor(private route: ActivatedRoute, private articlesService: BlogPostService) {
+  constructor(private route: ActivatedRoute, private articlesService: BlogPostService, private router: Router) {
 
   }
 
@@ -30,6 +30,10 @@ export class ArticleDetailsComponent implements OnInit {
     if(this.url){
       this.article$ = this.articlesService.getBlogPostByUrlHandle(this.url)
     }
+  }
+
+  onCancel() : void {
+    this.router.navigateByUrl('');
   }
 
 }
